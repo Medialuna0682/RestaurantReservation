ceee2724bb39eaf68042516fdb37fa67c90ab975
vivicaow <vivicaow@gmail.com>
2023-05-09 14:54:12 +1000

    confirmation page basic function added

--- RestaurantReservation/ConfirmationViewController.swift
+++ RestaurantReservation/ConfirmationViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ConfirmationViewController.swift
 //  RestaurantReservation
@@ -8,17 +9,38 @@
 import UIKit
 
 class ConfirmationViewController: UIViewController {
-    var restaurantName: String = "";
+    // 連接storyboard上的UI元素
+    @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var restaurantNameLabel: UILabel!
+    @IBOutlet weak var homeButton: UIButton! // 新增：返回主頁按鈕
+    
+    // 創建一個可選的Reservation實例
+    var reservation: Reservation? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // Hide back button
+        
+        // 初始化界面
+        setupView()
+    }
+    
+    // 設置界面的函數
+    private func setupView() {
+        // 隱藏返回按鈕
         self.navigationItem.hidesBackButton = true
         
-        restaurantNameLabel.text = restaurantName;
+        // 如果reservation不為nil，則更新餐廳名稱和預訂時間的標籤
+        if let reservation = reservation {
+            restaurantNameLabel.text = reservation.restaurant
+            timeLabel.text = reservation.time
+        } else {
+            restaurantNameLabel.text = "NIL"
+            timeLabel.text = "NIL"
+        }
     }
-
-
+    
+    // 新增：返回主頁按鈕的IBAction方法
+    @IBAction func homeButtonTapped(_ sender: UIButton) {
+        navigationController?.popToRootViewController(animated: true)
+    }
 }
-
