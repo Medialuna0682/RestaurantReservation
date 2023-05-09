7f4071ed240b126a8f6f98224760c31f361daaef
s3673832 <1042907721@qq.com>
2023-05-09 16:48:07 +1000

    gg

---
+++ RestaurantReservation/Restaurant.swift
@@ -0,0 +1,15 @@
+//
+//  Restaurant.swift
+//  RestaurantReservation
+//
+//  Created by Zeraporo on 9/5/2023.
+//
+
+import Foundation
+
+struct Restaurant {
+    let name: String
+    let location: String
+    let rating: Double
+    let cuisine: String
+}
--- RestaurantReservation/RestaurantViewController.swift
+++ RestaurantReservation/RestaurantViewController.swift
@@ -7,16 +7,35 @@
 
 import UIKit
 
-class RestaurantViewController: UIViewController {
+class RestaurantViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-  
+    @IBOutlet weak var tableView: UITableView!
+    let restaurantList = [
+           Restaurant(name: "餐厅一", location: "上海", rating: 4.5, cuisine: "川菜"),
+           Restaurant(name: "餐厅二", location: "北京", rating: 4.0, cuisine: "粤菜"),
+           Restaurant(name: "餐厅三", location: "广州", rating: 4.2, cuisine: "东北菜"),
+           Restaurant(name: "餐厅四", location: "深圳", rating: 3.8, cuisine: "湘菜"),
+       ]
+       
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // Hide back button
+        tableView.dataSource = self
+                tableView.delegate = self
         self.navigationItem.hidesBackButton = true
     }
-//hahahaha
+    @objc func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return restaurantList.count
+        }
+        
+    @objc(tableView:cellForRowAtIndexPath:) func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath)
+            
+            let restaurant = restaurantList[indexPath.row]
+            cell.textLabel?.text = restaurant.name
+            cell.detailTextLabel?.text = "\(restaurant.location) - \(restaurant.cuisine) - \(restaurant.rating)"
+            
+            return cell
+        }
+    }
 
-}
 
